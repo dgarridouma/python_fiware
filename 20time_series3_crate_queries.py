e585b752aa872b4ae83d08423764c1a366098865
dgarridomarquez <dgm@uma.es>
2021-04-11 19:35:28 +0200

    until example 20 time series

--- 20time_series3_crate_queries.py
+++ 20time_series3_crate_queries.py
@@ -31,8 +31,22 @@ print(response.text)
 
 print('Min over time period')
 newHeaders = {'fiware-service': 'openiot', 'fiware-servicepath': '/'}
-payload =  { 'aggrMethod': 'min','aggrPeriod': 'minute'}
-url = 'http://'+QUANTUMLEAP_HOST+':8668/v2/entities/urn:ngsi-ld:Termometer:001/attrs/motion'
+payload =  { 'aggrMethod': 'min','aggrPeriod': 'minute', 'limit':10} # limit 10
+url = 'http://'+QUANTUMLEAP_HOST+':8668/v2/entities/urn:ngsi-ld:Termometer:001/attrs/temperature'
 response=requests.get(url,headers=newHeaders,params=payload)
+response.encoding='utf-8'
+print(response.text)
+
+print('Latest N Sampled Values near a point')
+newHeaders = {'fiware-service': 'openiot', 'fiware-servicepath': '/'}
+url = 'http://'+QUANTUMLEAP_HOST+':8668/v2/types/Device/attrs/temperature?lastN=4&georel=near;maxDistance:5000&geometry=point&coords=40.25,-3.41'
+response=requests.get(url,headers=newHeaders)
+response.encoding='utf-8'
+print(response.text)
+
+print('Latest N Sampled Values in an area')
+newHeaders = {'fiware-service': 'openiot', 'fiware-servicepath': '/'}
+url = 'http://'+QUANTUMLEAP_HOST+':8668/v2/types/Device/attrs/temperature?lastN=4&&georel=coveredBy&geometry=polygon&coords=41,-5;35,-5;35,0;41,0;41,-5'
+response=requests.get(url,headers=newHeaders)
 response.encoding='utf-8'
 print(response.text)
