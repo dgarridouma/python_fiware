73f50de34cfddcd78a3e502acfdfe9800b71caba
dgarridomarquez <dgm@uma.es>
2021-04-11 21:51:59 +0200

    wirecloud examples

---
+++ 22wirecloud1_create_entities.py
@@ -0,0 +1,49 @@
+import requests
+import json
+import os
+
+ORION_HOST = os.getenv('ORION_HOST','localhost')
+
+# Create entity
+json_dict={
+    "id": "urn:ngsi-ld:Termometer:001",
+    "type": "Device",
+    "temperature": { "type": "Number", "value": 25.0},
+    "motion": { "type": "Number", "value": 0.0},
+    "location": {
+        "type": "geo:json",
+        "value": {
+             "type": "Point",
+             "coordinates": [-4.25, 36.43]
+        }
+    }
+}
+
+newHeaders = {'Content-type': 'application/json', 'Accept': 'application/json','fiware-service': 'openiot', 'fiware-servicepath': '/'}
+response = requests.post('http://'+ORION_HOST+':1026/v2/entities',
+                         data=json.dumps(json_dict),
+                         headers=newHeaders)
+print("Status code: ", response.status_code)
+
+
+# Create entity
+json_dict={
+    "id": "urn:ngsi-ld:Termometer:002",
+    "type": "Device",
+    "temperature": { "type": "Number", "value": 15.0},
+    "motion": { "type": "Number", "value": 0.0},
+    "location": {
+        "type": "geo:json",
+        "value": {
+             "type": "Point",
+             "coordinates": [-3.41, 40.25]
+        }
+    }
+}
+
+newHeaders = {'Content-type': 'application/json', 'Accept': 'application/json','fiware-service': 'openiot', 'fiware-servicepath': '/'}
+response = requests.post('http://'+ORION_HOST+':1026/v2/entities',
+                         data=json.dumps(json_dict),
+                         headers=newHeaders)
+print("Status code: ", response.status_code)
+
---
+++ 22wirecloud2_update_attr_temperature_motion_random.py
@@ -0,0 +1,56 @@
+import requests
+import json
+import os
+import random
+from time import sleep
+
+ORION_HOST = os.getenv('ORION_HOST','localhost')
+
+while True:
+  json_dict={
+  #    "id": "urn:ngsi-ld:Termometer:001",
+  #    "type": "Device",
+      "temperature": { "type": "Number", "value": random.randint(20,30)},
+      "motion": { "type": "Number", "value": random.randint(0,1)},
+#          "location": {
+#        "type": "geo:json",
+#        "value": {
+#             "type": "Point",
+#             "coordinates": [-4.25,36.43]
+#        }
+#    }
+
+  }
+  
+  newHeaders = {'Content-type': 'application/json', 'Accept': 'application/json', 'fiware-service': 'openiot', 'fiware-servicepath': '/'}
+  response = requests.post('http://'+ORION_HOST+':1026/v2/entities/urn:ngsi-ld:Termometer:001/attrs', data=json.dumps(json_dict), headers=newHeaders)
+  # success code - 204
+  print(response) 
+  
+  print(response.content) 
+  print(json_dict['temperature'],json_dict['motion'])
+  sleep(1) # BE CAREFUL with throtle in the subscription or the second termometer will not be stored
+
+  json_dict={
+  #    "id": "urn:ngsi-ld:Termometer:001",
+  #    "type": "Device",
+      "temperature": { "type": "Number", "value": random.randint(0,20)},
+      "motion": { "type": "Number", "value": random.randint(0,1)},
+#      "location": {
+#        "type": "geo:json",
+#        "value": {
+#             "type": "Point",
+#             "coordinates": [-3.41,40.25]
+#        }
+#     }
+
+  }
+  
+  newHeaders = {'Content-type': 'application/json', 'Accept': 'application/json', 'fiware-service': 'openiot', 'fiware-servicepath': '/'}
+  response = requests.post('http://'+ORION_HOST+':1026/v2/entities/urn:ngsi-ld:Termometer:002/attrs', data=json.dumps(json_dict), headers=newHeaders)
+  # success code - 204
+  print(response) 
+  
+  print(response.content) 
+  print(json_dict['temperature'],json_dict['motion'])
+  sleep(1)
