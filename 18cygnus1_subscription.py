4fc30e3e7297cbfc38799b050b831e9bb88c0436
dgarridomarquez <dgm@uma.es>
2021-05-03 20:27:41 +0200

    cygnus multi-db

--- 18cygnus1_subscription.py
+++ 18cygnus1_subscription.py
@@ -18,9 +18,9 @@ response = requests.post('http://'+ORION_HOST+':1026/v2/entities',
                          headers=newHeaders)
 print("Status code: ", response.status_code)
 
-# Create subscription
+# Create subscription (Mongo)
 json_dict={
-  "description": "Notify Cygnus of all context changes",
+  "description": "Notify Cygnus of all context changes Mongo",
   "subject": {
     "entities": [{"idPattern": ".*"}]
   },
@@ -46,3 +46,31 @@ print("Status code: ", response.status_code)
 # show collections
 # db["sth_/_urn:ngsi-ld:Termometer:001_Device"].find().limit(10);
 # db["sth_/_urn:ngsi-ld:Termometer:001_Device"].find({ attrName: "motion" , attrValue: 1 }).limit(10);
+
+
+# Create subscription (MySQL)
+json_dict={
+  "description": "Notify Cygnus of all context changes MySQL",
+  "subject": {
+    "entities": [{"idPattern": ".*"}]
+  },
+"notification": {
+    "http": {
+      "url": "http://cygnus:5050/notify"
+    }
+  },
+#  "throttling": 5
+}
+
+newHeaders = {'Content-type': 'application/json', 'fiware-service': 'openiot', 'fiware-servicepath': '/'}
+response = requests.post('http://'+ORION_HOST+':1026/v2/subscriptions',
+                         data=json.dumps(json_dict),
+                         headers=newHeaders)
+print("Status code: ", response.status_code)
+# Use next example to insert data and then:
+# docker exec -it db-mysql mysql -P 3306 -u root –psecret
+# show databases;
+# show schemas;
+# use openiot;
+# show tables;
+# select * from `urn_ngsi-ld_Termometer_001_Device`;
